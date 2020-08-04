9675e8bc5ca5d96414c52a34079d9a16d826e6b1
fakhrilak <fakhrilak23@gmail.com>
2020-08-05 00:04:01 +0700

    Updates

---
+++ precache-manifest.b12db0f56b49dd939749d4be1fc3daf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e71b4f4a23aba1919de36befe913495",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f270b149761e82d5e458",
+    "url": "/covid19/static/css/main.cb3d5bdb.chunk.css"
+  },
+  {
+    "revision": "e470fee4d89814a2cf37",
+    "url": "/covid19/static/js/2.735fbf49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.735fbf49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f270b149761e82d5e458",
+    "url": "/covid19/static/js/main.40787c67.chunk.js"
+  },
+  {
+    "revision": "04efb5c4b8685d54d57c",
+    "url": "/covid19/static/js/runtime-main.53f1daaa.js"
+  },
+  {
+    "revision": "274509f11beecf6943930d49720f5ac5",
+    "url": "/covid19/static/media/Thumbnail.274509f1.jpg"
+  }
+]);
