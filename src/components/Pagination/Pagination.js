374cad9e53e2e472677c0f1b7e2e0bbf9fdbb5be
fakhrilak <fakhrilak23@gmail.com>
2020-08-05 02:06:27 +0700

    covid19

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -37,7 +37,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, height}) => {
       <ul className='pagination'>
         {Filter.map(number => (
           <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='2' className='page-link'>
+            <a onClick={() => paginate(number)}  className='page-link'>
               {number}
             </a>
           </li>
